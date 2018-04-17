9db70b032834143f7d785388998e7cac3c266e30
Thomas Petitjean <contact@petitjeanthomas.fr>
2018-04-17 17:46:02 +0200

    version initial du projet hello-app

---
+++ src/rg/wcs/hello/SimpleHelloServlet.java
@@ -0,0 +1,80 @@
+package rg.wcs.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet(name = "SimpleHelloServlet", urlPatterns = {"/simple-hello"})
+public class SimpleHelloServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userName = request.getParameter("userName");
+        String userSurname = request.getParameter("userSurname");
+        String timeUser = request.getParameter("time");
+        String customMsg = "";
+        PrintWriter writer = response.getWriter();
+
+        //gestion du temps
+        DateFormat sdf = new SimpleDateFormat("hh:mm");
+        Date date=null;
+        try {
+            date = sdf.parse(timeUser);
+            customMsg = getCustomMsgByHour(date);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // build HTML code
+//        String htmlRespone = "<html>";
+//        htmlRespone += "<h2>"+customMsg+" "+userName+ " " + userSurname + ". How are you ?</h2>";
+//        htmlRespone += "</html>";
+//        writer.println(htmlRespone);
+
+
+        request.setAttribute("name", userName);
+        request.setAttribute("familyName", userSurname);
+        request.setAttribute("customMsg", customMsg);
+        request.getRequestDispatcher("custom-hello.jsp").forward(request, response);
+    }
+
+    protected String getCustomMsgByHour (Date timeUser) {
+
+        DateFormat sdf = new SimpleDateFormat("hh:mm");
+        Date morningHours=null;
+        Date afternoonHours=null;
+        Date eveningHours=null;
+        String msg = "soucis avec ton code !";
+        try {
+            morningHours = sdf.parse("05:00");
+            afternoonHours = sdf.parse("12:00");
+            eveningHours = sdf.parse("18:00");
+
+            if(timeUser.before(morningHours) || eveningHours.before(timeUser)){
+                msg =  "Good Evening";
+            } else if (timeUser.before(afternoonHours)) {
+                msg =  "Good Morning";
+            } else {
+                msg =  "Good Afternoon";
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    return msg;
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //PrintWriter out = response.getWriter(); out.print("Simple hello coucou!");
+
+
+
+    }
+}
---
+++ web/custom-hello.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: thomas
+  Date: 17/04/18
+  Time: 15:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+
+    <body>
+
+        <h3> ${customMsg} ${name} ${familyName} </h3>
+
+    </body>
+</html>
